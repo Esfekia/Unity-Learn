a2eb87f20b4b16438bb1ea170a7d00f4e35a394d
Sertac <sertac@gmail.com>
2022-04-23 07:55:55 -0400

    Horizontal Boundaries added
    X axis boundaries to Player's movement have been added and xBound variable defined.

--- Create With Code - Carl D/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create With Code - Carl D/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Check for left and right horizontal boundaries for player
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        //Move player horizontally according to player input
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
