dafc284c44d137f99e7898860b0317b47394c4ee
Sertac <sertac@gmail.com>
2022-04-26 14:20:10 -0400

    Added 3 dimensional rotation as well as random color every frame.
    Added 3 dimensional rotation as well as random color every frame. Published it as OpenGL on unity as part of the tutorial.

--- Create With Code - Carl D/Mod The Cube/Assets/ModTheCube/Cube.cs
+++ Create With Code - Carl D/Mod The Cube/Assets/ModTheCube/Cube.cs
@@ -7,23 +7,32 @@ public class Cube : MonoBehaviour
     public MeshRenderer Renderer;
     public float moveSpeed = 0.1f;
     public int rotateSpeed = 2;
+
     //public float growSpeed = 0.1f;
-    
+
     void Start()
     {
         transform.position = new Vector3(3, 4, 1);
         transform.localScale = Vector3.one * 1.3f;
-        
+
         Material material = Renderer.material;
-        
+
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
     }
-    
+
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime * rotateSpeed, 0.0f, 0.0f);
-        transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        transform.Rotate(2.0f * Time.deltaTime * rotateSpeed, 0.0f, 0.0f);
+        transform.Rotate(0.0f, 2.0f * Time.deltaTime * rotateSpeed, 0.0f);
+        transform.Rotate(0.0f, 0.0f, 2.0f * Time.deltaTime * rotateSpeed);
+        //transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
         //transform.localScale = Vector3.one * growSpeed;
         //growSpeed +=0.1f;
+        /*public float redRange = Random.Range(0.0f, 1.0f);
+        public float greenRange = Random.Range(0.0f, 1.0f);
+        public float blueRange = Random.Range(0.0f, 1.0f);
+        public float transRange = Random.Range(0.0f, 1.0f);*/
+        Material material = Renderer.material;
+        material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
     }
 }
