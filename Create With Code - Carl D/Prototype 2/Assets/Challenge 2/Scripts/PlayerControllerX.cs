eae1d960cf776f93b5776b789c4d22d86f59788b
Sertac <sertac@gmail.com>
2022-04-26 09:05:34 -0400

    Added a cooldown to spawning the dog.
    Added a cooldown to spawning the dog.

--- Create With Code - Carl D/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create With Code - Carl D/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float dogOut = 0.5f;
+    public float dogIn = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > dogIn)
         {
+            dogIn = Time.time + dogOut;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
