a5b5098e58cdc799c8ff67f04802eecbbdf54ae6
Sertac <sertac@gmail.com>
2022-04-27 15:33:35 -0400

    Background repeater timer fixed. Added gameover trigger to hitting obstacles
    Console now displays game over when player hits an obstacle.

--- Create With Code - Carl D/Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Create With Code - Carl D/Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     public Vector3 startPos;
-    public float backgroundAdjust = 56.2f;
+    public float repeatWidth;
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - backgroundAdjust)
+        if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
         }
