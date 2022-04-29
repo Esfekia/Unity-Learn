7c9bb259a1e132774dbddebc6f3003b8d4fb88b3
Sertac <sertac@gmail.com>
2022-04-29 09:47:02 -0400

    PlayerController() now correctly controls the balloon when space is pressed
    Added missing GetComponent<RigidBody>() to the script, tweaked the gravity and floatforce values for a good balance.

--- Create With Code - Carl D/Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Create With Code - Carl D/Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -6,8 +6,8 @@ public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver;
 
-    public float floatForce;
-    private float gravityModifier = 1.5f;
+    public float floatForce = 0.25f;
+    private float gravityModifier = 1.0f;
     private Rigidbody playerRb;
 
     public ParticleSystem explosionParticle;
@@ -21,6 +21,7 @@ public class PlayerControllerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
 
@@ -35,7 +36,7 @@ public class PlayerControllerX : MonoBehaviour
         // While space is pressed and player is low enough, float up
         if (Input.GetKey(KeyCode.Space) && !gameOver)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
         }
     }
 
