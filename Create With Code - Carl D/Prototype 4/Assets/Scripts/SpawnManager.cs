68fac150e0d178d0557255bfc4c851a2ec787b66
Sertac <sertac@gmail.com>
2022-05-06 11:51:00 -0400

    added a waveCount, which is now the number of enemies we spawn (per wave)
    added a waveCount, which is now the number of enemies we spawn (per wave)

--- Create With Code - Carl D/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Create With Code - Carl D/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,12 @@ public class SpawnManager : MonoBehaviour
     private float spawnRange = 9.0f;
 
     public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(5);        
+        SpawnEnemyWave(1);        
     }
 
     // Update is called once per frame
@@ -21,7 +22,8 @@ public class SpawnManager : MonoBehaviour
         enemyCount = FindObjectsOfType<Enemy>().Length;
         if (enemyCount == 0)
         {
-            SpawnEnemyWave(1);
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
         }
 
     }
