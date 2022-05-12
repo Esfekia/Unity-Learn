c86ad0fe65e100e3f82699d24e78dd89faefa986
Sertac <sertac@gmail.com>
2022-05-12 15:37:12 -0400

    Added a coroutine in start to instantiate our targets using a while loop and waitforseconds.
    Added a coroutine in start to instantiate our targets using a while loop.

--- Create With Code - Carl D/Prototype 5/Assets/Scripts/Target.cs
+++ Create With Code - Carl D/Prototype 5/Assets/Scripts/Target.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-    private float minSpeed = 12.0f;
+    private float minSpeed = 13.0f;
     private float maxSpeed = 16.0f;
     private float maxTorque = 10;
     private float xRange = 4.0f;
