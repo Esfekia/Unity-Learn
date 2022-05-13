5d0de0bc5547201bc8548c3cf7e64e6953e99ee5
Sertac <sertac@gmail.com>
2022-05-13 15:25:47 -0400

    Score variable is now tracked and testrun to see if it gets added is succesful. (need to remove it from instantiating method to somewhere later)
    Score variable is now tracked and testrun to see if it gets added is succesful.

--- Create With Code - Carl D/Prototype 5/Assets/Scripts/GameManager.cs
+++ Create With Code - Carl D/Prototype 5/Assets/Scripts/GameManager.cs
@@ -14,7 +14,8 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score: " + score;
+        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -30,6 +31,13 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            UpdateScore(5);
         }
+    }
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+
     }
 }
