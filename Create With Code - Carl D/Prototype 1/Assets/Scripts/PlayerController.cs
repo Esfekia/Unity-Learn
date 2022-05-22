83fedb22fadc5d903b45498311633fa08abf61a2
Sertac <sertac@gmail.com>
2022-05-22 07:07:00 -0400

    Prototype 2 updated according to Lesson 6.2 - Research and Troubleshooting
    - Changed movement from transform to AddRelativeForce.
    - Changed mesh colliders on the wheels to wheel colliders.
    - Added new center of mass emptyobject to ensure car doesnt flip/fly with force.
    - Adjusted mass, speed and position to enable the car to move with the new center of mass.
    - Added a Speedometer for each player which now correctly displays the speed in mph.

--- Create With Code - Carl D/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create With Code - Carl D/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Protected allows the variable or method to be accessible from outside this script, but only those which inherit the class in which it is.
-    [SerializeField] protected float speed = 20.0f;
+    //[SerializeField] protected float speed = 20.0f;
 
     // Variables that are declared as const cannot later be changed in the script anywhere else.
     private const float turnSpeed = 45.0f;
+    [SerializeField]private float horsePower = 0;
     private float horizontalInput;
     private float forwardInput;
     public string inputID;
+    private Rigidbody playerRb;
+    public GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] private float speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -26,8 +35,15 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical" + inputID);
 
         //Move the vehicle forward.
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //Apply force in forward direction.
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
+
         //Rotate the vehicle based on horizontal input
-        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);        
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        speed = playerRb.velocity.magnitude * 2.237f; // 3.6 for km/h
+        speedometerText.SetText("Speed: " + Mathf.RoundToInt(speed) + "m/h");
     }
 }
