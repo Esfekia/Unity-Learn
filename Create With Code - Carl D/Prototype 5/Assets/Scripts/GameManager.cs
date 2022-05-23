2e4b7a56841976a5a90437656bfb3d2a8b737b78
Sertac <sertac@gmail.com>
2022-05-23 06:17:03 -0400

    Added a custom green cursor for more visibility on the white background.
    Added a custom green cursor for more visibility on the white background.

--- Create With Code - Carl D/Prototype 5/Assets/Scripts/GameManager.cs
+++ Create With Code - Carl D/Prototype 5/Assets/Scripts/GameManager.cs
@@ -15,11 +15,13 @@ public class GameManager : MonoBehaviour
     public bool gameIsActive;
     private int score;
     private float spawnRate = 2.0f;
+    public Texture2D cursorArrow;
+
     // Start is called before the first frame update
     void Start()
     {
-  
-        
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+
     }
 
     // Update is called once per frame
@@ -63,5 +65,10 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
         titleScreen.gameObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
