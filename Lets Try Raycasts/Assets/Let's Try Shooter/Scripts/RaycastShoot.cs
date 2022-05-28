5c1ecea3379520dfde3ad853d75d0b979f9a81f0
Esfekia <sertac@gmail.com>
2022-05-28 07:50:16 -0400

    added line renderer and script elements to check if a raycast has hit a collider
    added line renderer and script elements to check if a raycast has hit a collider

--- Lets Try Raycasts/Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
+++ Lets Try Raycasts/Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -46,6 +46,65 @@ public class RaycastShoot : MonoBehaviour
 
     void Update()
     {
+        // check if fire button is pressed and if enough time passed since last fire.
+        if (Input.GetButtonDown ("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+
+            // we need to set the origin of the ray in relation to the camera's center.
+            // take a position relative to the camera and convert it to world space.
+            // viewport is the area we see on screen an is limited to (0,0) and (1,1)
+            // by positioning it at (0.5), (0.5) we center it exactly in the middle of the "screen"
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
+            // determine if the ray has hit a gameobject with a collider attached and store it in variable:
+            RaycastHit hit;
+
+            // determine start and end positions for laser line when player fires:
+            // establish initial position of the line and target.
+            laserLine.SetPosition(0, gunEnd.position);
+
+            // we will use physics.raycast to cast our ray
+            // we will use the result of raycast to determine where the end point of our laserline will be
+            // later we will also use it to apply physics forces to gameobjects it intersected and
+            // to deal damage if it has health attached
+            // physics.raycast returns a boolean
+
+
+            // first the origin, then the dfirection.
+            // using the out keyword allows us to store information from a function in addition to its return type
+            // finally the weapon range
+
+            if (Physics.Raycast(rayOrigin,fpsCam.transform.forward, out hit, weaponRange))
+            // two possible positions for the end of our laser: the position of whatever we hit or nothing.
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            // 50 units away from origin, in the direction of our camera:
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+            // now the linerenderer will draw a line from the position of the gun end to the position that the player aimed at
+            // using our raycast
+
+            // (we add the line renderer component to the Gun gameobject and set its Gun End using editor)
+        }    
         
+
+    }
+
+    // turn on and off our laser fire effect with a coroutine.
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+
+        // wait for 0.75 seconds before turning the laser line off:
+        yield return shotDuration;
+        laserLine.enabled = false;
+
     }
 }
